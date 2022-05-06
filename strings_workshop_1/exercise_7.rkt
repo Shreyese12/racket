74d6d18150cb4d36f7b53c63ccaa58fd20a71943
Samuel Reyes <shreyese12@gmail.com>
2022-05-05 21:11:50 -0500

    Exercise 7 done

--- strings_workshop_1/exercise_7.rkt
+++ strings_workshop_1/exercise_7.rkt
@@ -1,3 +1,18 @@
 #lang racket
 
+(define (Main)
+    (define str_1 0)
+    (define str_2 0)
 
+    (printf "Please enter first string: ")
+    (set! str_1 (read-line))
+    (printf "Please enter second string: ")
+    (set! str_2 (read-line))
+
+    (if (string<=? str_1 str_2)
+        (printf "~a is less than ~a\n" str_1 str_2)
+        (printf "~a is less than ~a\n" str_2 str_1)
+    )
+)
+
+(Main)
