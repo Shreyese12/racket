8b576d83ad59bf3b1d1e117763f8e0b21aaa36cf
Samuel Reyes <shreyes12@gmail.com>
2022-05-16 22:15:49 -0500

    I hate racket

--- lists_workshop_1/compare_lists.rkt
+++ lists_workshop_1/compare_lists.rkt
@@ -1,48 +1,38 @@
 #lang racket
 
-(define (Fill i l)
-    (if (> i 0)
-        (Fill (- i 1) (cons (random 0 3) l))
-        l
+(define (ListLength i l)
+    (if (null? l)
+        i
+        (ListLength (+ i 1) (cdr l))
     )
 )
 
-(define (CompareLists i l1 l2 index)
-    (if (> i 0)
-        (if (= (car l1) (car l2))
-            (CompareLists (- i 1) (cons (cdr l1) l1) (cons (cdr l2) l2) 1)
-            (set! index 0)
+(define (Compare i len l1 l2 m)
+    (if (< i len)
+        (if (not (= (car l1) (car l2)))
+            (set! m 0)
+            (begin
+                (set! m 1)
+                (printf "~a\n" l1)
+                (printf "~a\n" l2)
+                (Compare (+ i 1) len (cdr l1) (cdr l2) m)
+            )
         )
         (void)
     )
-    index
+    m
 )
 
 (define (Main)
     (define size 0)
-    (define l1 (list))
-    (define l2 (list))
+    (define l1 (list 1 40 3 4))
+    (define l2 (list 1 2 3 4))
 
-    (printf "Please enter the number of initial values of the lists: ")
-    (set! size (read))
-
-    (if (and (> size 0) (> size 0))
-        (begin
-            (set! l1 (Fill size l1))
-            (set! l2 (Fill size l2))
-            (printf "~a\n" l1)
-            (printf "~a\n" l2)
-            (if (= (CompareLists size l1 l2 0) 1)
-                (printf "There are the same.\n")
-                (printf "There aren't the same.\n")
-            )
-        )
-        (begin
-            (printf "Invalid values.")
-            (newline)
-            (Main)
-        )
-    )
+    (printf "List 1: \n~a\n" l1)
+    (newline)
+    (printf "List 2: \n~a\n" l2)
+    (newline)
+    (Compare 0 (ListLength 0 l2) l1 l2 0)
 )
 
 (Main)
